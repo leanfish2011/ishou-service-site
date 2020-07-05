1d8b75e11fbe792d2fe50209d300c6d28d9922af
yangtzeyufei <yangtze_yufei@163.com>
2020-07-05 10:28:27 +0800

    feat(feign):首页feignclient

---
+++ site-sdk/src/main/java/com/tim/ishou/site/feign/SiteFeignClient.java
@@ -0,0 +1,16 @@
+package com.tim.ishou.site.feign;
+
+import com.tim.config.FeignConfiguration;
+import com.tim.ishou.site.vo.SiteHomeSearchData;
+import com.tim.message.Message;
+import org.springframework.cloud.netflix.feign.FeignClient;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+@FeignClient(name = "${site.server.name:ishou-service-site}", path = "${site.api.version.module:/api/v1/site/index}", configuration = FeignConfiguration.class)
+public interface SiteFeignClient {
+
+  @RequestMapping(method = RequestMethod.GET)
+  Message<SiteHomeSearchData> list();
+
+}
