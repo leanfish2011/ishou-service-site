1d34bf359bfd969ad7032cf8af8f358abe29ec31
yangtzeyufei <yangtze_yufei@163.com>
2021-01-16 17:13:17 +0800

    feat(site):增加标签、备注字段长度

--- mariadb/ishou_site.sql
+++ mariadb/ishou_site.sql
@@ -13,9 +13,9 @@ CREATE TABLE `site_home` (
   `modified_time` datetime DEFAULT NULL ON UPDATE current_timestamp() COMMENT '修改时间',
   `modifier_id` varchar(60) DEFAULT NULL COMMENT '修改人id',
   `name` varchar(50) DEFAULT NULL COMMENT '网站名称',
-  `url` varchar(100) DEFAULT NULL COMMENT '网站地址',
-  `remark` varchar(100) DEFAULT NULL COMMENT '简介',
-  `tag` varchar(100) DEFAULT NULL COMMENT '标签',
+  `url` varchar(200) DEFAULT NULL COMMENT '网站地址',
+  `remark` varchar(500) DEFAULT NULL COMMENT '简介',
+  `tag` varchar(500) DEFAULT NULL COMMENT '标签',
   `sort_num` int(11) DEFAULT NULL COMMENT '顺序',
   `visit_count` int(11) DEFAULT 0 COMMENT '访问次数',
   PRIMARY KEY (`id`)
@@ -30,9 +30,9 @@ CREATE TABLE `site_personal` (
   `modified_time` datetime DEFAULT NULL ON UPDATE current_timestamp() COMMENT '修改时间',
   `modifier_id` varchar(60) DEFAULT NULL COMMENT '修改人id',
   `name` varchar(50) DEFAULT NULL COMMENT '网站名称',
-  `url` varchar(100) DEFAULT NULL COMMENT '网站地址',
-  `remark` varchar(100) DEFAULT NULL COMMENT '简介',
-  `tag` varchar(100) DEFAULT NULL COMMENT '标签',
+  `url` varchar(200) DEFAULT NULL COMMENT '网站地址',
+  `remark` varchar(500) DEFAULT NULL COMMENT '简介',
+  `tag` varchar(500) DEFAULT NULL COMMENT '标签',
   `sort_num` int(11) DEFAULT NULL COMMENT '顺序',
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COMMENT='个人网站收藏';
--- mariadb/update.sql
+++ mariadb/update.sql
@@ -15,3 +15,16 @@ CHANGE COLUMN `url` `url` VARCHAR(200) NULL DEFAULT NULL COMMENT '网站地址' 
 -- 2020-06-20，个人网站url字段加长
 ALTER TABLE `ishou_site`.`site_personal`
 CHANGE COLUMN `url` `url` VARCHAR(200) NULL DEFAULT NULL COMMENT '网站地址' ;
+
+-- 2021-01-16，标签、备注增加长度
+ALTER TABLE `ishou_site`.`site_personal`
+CHANGE COLUMN `tag` `tag` VARCHAR(500) NULL DEFAULT NULL COMMENT '标签' ;
+
+ALTER TABLE `ishou_site`.`site_personal`
+CHANGE COLUMN `remark` `remark` VARCHAR(500) NULL DEFAULT NULL COMMENT '简介' ;
+
+ALTER TABLE `ishou_site`.`site_home`
+CHANGE COLUMN `tag` `tag` VARCHAR(500) NULL DEFAULT NULL COMMENT '标签' ;
+
+ALTER TABLE `ishou_site`.`site_home`
+CHANGE COLUMN `remark` `remark` VARCHAR(500) NULL DEFAULT NULL COMMENT '简介' ;
