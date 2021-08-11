9b0e22abb6f978a8acd06be3a4c5d7cee287d08a
yangtzeyufei <yangtze_yufei@163.com>
2021-08-11 22:30:45 +0800

    feat(sdk):修改sdk url

--- site-sdk/src/main/java/com/tim/ishou/site/feign/SiteFeignClient.java
+++ site-sdk/src/main/java/com/tim/ishou/site/feign/SiteFeignClient.java
@@ -7,7 +7,7 @@ import org.springframework.cloud.netflix.feign.FeignClient;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
-@FeignClient(name = "${site.server.name:ishou-service-site}", path = "${site.api.version.module:/api/v1/site/index}", configuration = FeignConfiguration.class)
+@FeignClient(name = "${site.server.name:ishou-service-site}", path = "${site.api.version.module:/api/ishou/v2/site/index}", configuration = FeignConfiguration.class)
 public interface SiteFeignClient {
 
   @RequestMapping(method = RequestMethod.GET)
